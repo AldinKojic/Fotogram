1ceee38e4b0eade317de1f3db8829005bd56450f
AldinKojic <aldin.kojic1809@gmail.com>
2025-11-13 22:29:52 +0100

    new code

--- script.js
+++ script.js
@@ -1,71 +1,27 @@
-// Elemente referenzieren
-const modal = document.getElementById("myModal");
-const modalImg = document.getElementById("modalImg");
-const closeBtn = document.querySelector(".close");
-const counterEl = document.getElementById("whichPic");
-const prevBtn = document.querySelector(".btn-prev");
-const nextBtn = document.querySelector(".btn-next");
+let pics = [
+  "./Img2/Schaf.jpg",
+  "./Img2/Viper-Schlange.jpg",
+"./Img2/Wolf.jpg",
+"./Img2/Husky.jpg",
+"./Img2/Löwe.png",
+"./Img2/Eule.jpg",
+"./Img2/Marienkäfer - Kopie.jpg",
+"./Img2/Giraffe - Kopie.jpg",
+"./Img2/brown-bear-892232_1280.jpg",
+"./Img2/Schmetterling.jpg",
+"./Img2/Adler.jpg"
+]
 
-// Alle Galerie-Bilder einsammeln
-const images = Array.from(document.querySelectorAll(".gallery-img"));
-let currentIndex = 0;
+function showpic() {
+  let contentRef = document.getElementById('imgList');
+  
+ for (let indexPics = 0; indexPics < pics.length; indexPics++) {
+  const element = pics[indexPics];
+    const img = document.createElement("img");
+    img.src = element;
+    
 
-// Anzeige im Modal aktualisieren
-function updateModal() {
-  const img = images[currentIndex];
-  modalImg.src = img.src;
-  modalImg.alt = img.alt || "";
-  counterEl.textContent = `${currentIndex + 1}/${images.length}`;
-
-  // optionales Preloading für flüssigen Wechsel
-  const nextIdx = (currentIndex + 1) % images.length;
-  const prevIdx = (currentIndex - 1 + images.length) % images.length;
-  new Image().src = images[nextIdx].src;
-  new Image().src = images[prevIdx].src;
+    contentRef.appendChild(img);
+ }
 }
-
-// Modal öffnen mit gewähltem Index
-function openModal(index) {
-  currentIndex = index;
-  updateModal();
-  modal.style.display = "block";
-  modal.setAttribute("aria-hidden", "false");
-}
-
-// Modal schließen
-function closeModal() {
-  modal.style.display = "none";
-  modal.setAttribute("aria-hidden", "true");
-}
-
-// Klick-Events für alle Thumbnails
-images.forEach((img, idx) => {
-  img.addEventListener("click", () => openModal(idx));
-});
-
-// Weiter / Zurück
-nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % images.length;
-  updateModal();
-});
-
-prevBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  updateModal();
-});
-
-// Schließen (X), Klick auf Hintergrund, und Tastatur
-closeBtn.addEventListener("click", closeModal);
-
-modal.addEventListener("click", (e) => {
-  if (e.target === modal) closeModal();
-});
-
-document.addEventListener("keydown", (e) => {
-  if (modal.style.display === "block") {
-    if (e.key === "Escape") closeModal();
-    if (e.key === "ArrowRight") nextBtn.click();
-    if (e.key === "ArrowLeft") prevBtn.click();
-  }
-});
-
+showpic();
