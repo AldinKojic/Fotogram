efd03563d76352f3f998ce693224d3ca24d09335
AldinKojic <aldin.kojic1809@gmail.com>
2025-12-07 21:53:25 +0100

    new lion img + semantisch

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let pics = [
   "./Img2/Viper-Schlange.jpg",
   "./Img2/Wolf.jpg",
   "./Img2/Husky.jpg",
-  "./Img2/Löwe.png",
+  "./Img2/Löwe.jpg",
   "./Img2/Eule.jpg",
   "./Img2/Marienkäfer - Kopie.jpg",
   "./Img2/Giraffe - Kopie.jpg",
@@ -25,10 +25,21 @@ let animalNames = [
   "Schmetterling",
 ];
 
-let numbers = ["1/10", "2/10", "3/10", "4/10", "5/10", "6/10", "7/10", "8/10", "9/10", "10/10"];
+let numbers = [
+  "1/10",
+  "2/10",
+  "3/10",
+  "4/10",
+  "5/10",
+  "6/10",
+  "7/10",
+  "8/10",
+  "9/10",
+  "10/10",
+];
 
 function renderImages() {
-  let contentRef = document.getElementById("imgList");
+  let contentRef = document.getElementById("gallery-list");
   contentRef.innerHTML = "";
   for (let indexPics = 0; indexPics < pics.length; indexPics++) {
     contentRef.innerHTML += imgTemplates(indexPics);
@@ -43,50 +54,46 @@ function imgTemplates(indexPics) {
 }
 
 function openDialog(indexPics) {
-  const dialogRef = document.getElementById("myDialog");
+  const dialogRef = document.getElementById("dialog-photo");
   dialogRef.showModal();
-  const modalPicture = document.getElementById("modal-picture");
+  const modalPicture = document.getElementById("dialog-image-src");
   currentIndex = indexPics;
   modalPicture.src = pics[currentIndex];
-  const animalRef = document.getElementById("animals-names");
+  const animalRef = document.getElementById("dialog-title-text");
   animalRef.innerHTML = animalNames[currentIndex];
-  const numbersRef = document.getElementById("site-number");
+  const numbersRef = document.getElementById("dialog-index");
   numbersRef.innerHTML = numbers[currentIndex];
+  
 }
 function nextPicture() {
-  const modalPicture = document.getElementById("modal-picture");
-  const animalRef = document.getElementById("animals-names");
-  const numbersRef = document.getElementById("site-number");
+  const modalPicture = document.getElementById("dialog-image-src");
+  const animalRef = document.getElementById("dialog-title-text");
+  const numbersRef = document.getElementById("dialog-index");
   currentIndex++;
   if (currentIndex >= pics.length) currentIndex = 0;
   modalPicture.src = pics[currentIndex];
-  animalRef.innerHTML = animalNames[currentIndex];
-
+  animalRef.innerHTML =   animalNames[currentIndex];
   numbersRef.innerHTML = numbers[currentIndex];
+  
 }
 
 function previousPicture() {
-  const modalPicture = document.getElementById("modal-picture");
-  const animalRef = document.getElementById("animals-names");
-  const numbersRef = document.getElementById("site-number");
+  const modalPicture = document.getElementById("dialog-image-src");
+  const animalRef = document.getElementById("dialog-title-text");
+  const numbersRef = document.getElementById("dialog-index");
   currentIndex--;
-  if (currentIndex < 0) currentIndex = 0;
+  if (currentIndex < 0) currentIndex = pics.length - 1;
   modalPicture.src = pics[currentIndex];
   animalRef.innerHTML = animalNames[currentIndex];
-
   numbersRef.innerHTML = numbers[currentIndex];
 }
 
-// function nextPicture() {
-//   const PictureRef = document.getElementById("weiter");
-//   PictureRef.innerHTML = "";
+function closeDialog() {
+ const closeRef = document.getElementById("dialog-photo");
+  closeRef.close();
+ 
+}
 
-//   while (pics < pics.length) {
-//     PictureRef.innerHTML += renderImages(indexPics);
-//     pics++;
-//   }
-// }
-
-// // function nextPicture(){
-// //  PictureRef.innerHTML+= imgTemplates(indexPics);
-// // }
+function prevBubbiling(event){
+   event.stopPropagation(event);
+}
